672c311ea12bb973b7e3d715b435527191e231a7
Lance Johnson <rlancejohnson@gmail.com>
2020-06-19 23:39:39 -0600

    added static dir and update api

--- src/server/server.go
+++ src/server/server.go
@@ -21,7 +21,19 @@ func GetPort(defaultPort string) string {
 }
 
 func TitlePage(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Go Data Server!\n")
-    fmt.Fprintf(w, "----------------\n")
-    fmt.Fprintf(w, r.URL.Path[1:])
+    if r.URL.Path[1:] != "" {
+        file, err := ioutil.ReadFile(r.URL.Path[1:])
+        printErr(err)
+
+        fmt.Fprintf(w, string(file))
+
+    } else {
+        fmt.Fprintf(w, "Please append the url with a file name.")
+    }
+}
+
+func printErr(err error) {
+    if err != nil {
+        fmt.Println(err)
+    }
 }
